03a071e0be062c331c726da3473fcf878710c2da
KontorPC <KontorPC@DESKTOP-47J6C0I>
2021-04-04 11:02:13 +0200

    another small lambda test

---
+++ TryingOutThisNThat/src/lambdaTest/LambdaFuncInterface2.java
@@ -0,0 +1,5 @@
+package lambdaTest;
+
+public interface LambdaFuncInterface2 {
+	public boolean olderThan(Person person1, Person person2);
+}
--- TryingOutThisNThat/src/lambdaTest/LambdaMain.java
+++ TryingOutThisNThat/src/lambdaTest/LambdaMain.java
@@ -1,16 +1,24 @@
 package lambdaTest;
 
-public class LambdaMain {
-	Integer a =1;
-	Integer b =2;
 
+public class LambdaMain {
+	static int a = 1, b =2;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		LambdaFuncInterface lambdaFuncInterface = (x, y) ->  x + y; 
 		
-		Integer xx = lambdaFuncInterface.summer(2, 2);
+		Integer xx = lambdaFuncInterface.summer(a, b);
 		
 		System.out.println(xx);
+		
+		
+		Person personX = new Person("Svend", 42);
+		Person personY = new Person("Knud", 53);
+		LambdaFuncInterface2 lambdaFuncInterface2 = (person1, person2) -> person1.getAge() > person2.getAge();
+		
+//		boolean xOlderThanY = lambdaFuncInterface2.olderThan(personX, personY);
+		System.out.println(lambdaFuncInterface2.olderThan(personX, personY));
 	
 
 	}
